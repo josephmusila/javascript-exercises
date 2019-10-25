6b4cdb7868c7b0601e675e870a0546120432e714
joseph musila <josemusila03@gmail.com>
2019-10-25 20:39:28 +0300

    initial commit

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -88,7 +88,7 @@ describe("basics", function() {
             // write all the zeros.
             // Write it as a string so that the .length property can be checked.
             // ✏️ EDIT NEXT LINE ✏️
-            const bigNumber = "e someVariable"
+            const bigNumber = ""
             ////////////////////////////////////////////////////////////////////
 
             expect(typeof bigNumber).to.equal("string");
